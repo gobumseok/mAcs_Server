9d92fc50219d57855e441aa8ef284350019a09c4
beom seok ko <go2401@gmail.com>
2023-03-08 20:11:59 +0900

    amrtype post test

--- src/control/Amrtype.controller.ts
+++ src/control/Amrtype.controller.ts
@@ -8,29 +8,42 @@ import {
     Res,
     Response,
     Body,
+    Req,
+    Logger
   } from '@nestjs/common';
-import { Console } from 'console';
+
 
 import { AmrtypeDto } from '../dto/Amrtype.dto';
 import { AmrtypeService } from '../service/Amrtype.service';
 
 @Controller('Amrtype')
 export class AmrtypeController {
-
+  private readonly logger = new Logger(AmrtypeController.name);
   constructor(private amrtypeService: AmrtypeService){
      this.amrtypeService = amrtypeService;
   }
   
   //amr type 등록
   @Post()
-  async Amrtype(@Body() armType : AmrtypeDto) : Promise<string>{
+  async Amrtype(@Body() armType) : Promise<void>{
+    //data check
+    this.logger.debug(armType)
     
-    await this.amrtypeService.Save(armType);
-    return Object.assign({
-      data: { ...armType },
-      statusCode: 201,
-      statusMsg: `saved successfully`,
-    });
+    const amrPtype = new AmrtypeDto();
+    amrPtype.id = armType['id'];
+    amrPtype.description = armType['description'];
+    amrPtype.createdAt = new Date(armType['createdAt']);
+    amrPtype.updatedAt = new Date(armType['updatedAt']);
+
+
+    this.logger.debug(amrPtype.createdAt);
+    return armType;
+    //await this.amrtypeService.Save(armType);
+    //return Object.assign({
+    //  data: { ...armType },
+    //  statusCode: 201,
+    //  statusMsg: `saved successfully`,
+    //});
   }
 
   //create(@Body() createPostDto: CreatePostDto, @Res() res: Response) {
