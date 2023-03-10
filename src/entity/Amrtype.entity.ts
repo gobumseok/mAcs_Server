9604550c7ed0a3237378c34981b49e8ef5301426
beom seok ko <go2401@gmail.com>
2023-03-10 15:07:09 +0900

    orm 구조 변경

--- src/entity/Amrtype.entity.ts
+++ src/entity/Amrtype.entity.ts
@@ -1,17 +1,15 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { AmrEntity } from './Amr.entity';
 import { CallorderEntity } from './Callorder.entity';
 
 @Entity("amrtype",{schema:'acs'})
 export class AmrtypeEntity {
-  @PrimaryGeneratedColumn({ type: "bigint", name: "id" })
-  id: string;
+  
+  @PrimaryColumn("varchar", { name: "type_id",unique:true,length: 30 })
+  type_id: string;
 
   @Column("varchar", { name: "code", length: 5 })
   code: string;
-
-  @Column("varchar", { name: "type_id",unique:true,length: 30 })
-  type_id: string;
 
   @Column("varchar", { name: "description", nullable: true, length: 50 })
   description: string | null;
